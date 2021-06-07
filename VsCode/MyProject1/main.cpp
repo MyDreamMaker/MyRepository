84f6f41dd536796b684221d66b7ef6657314dadf
zhouxuzhou <895703037@qq.com>
2021-06-07 14:37:37 +0800

    Signed-off-by: zhouxuzhou <895703037@qq.com>

---
+++ VsCode/MyProject1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "swap.h"
+using namespace std;
+
+int main(int argc,char** argv)
+{
+    int val1 = 10;
+    int val2 = 20;
+    cout<<"before swap:"<<endl;
+    cout<<"val1:"<<val1<<endl;
+    cout<<"val2:"<<val2<<endl;
+    swap(val1,val2);
+
+    cout<<"after swap:"<<endl;
+    return 0;
+}
